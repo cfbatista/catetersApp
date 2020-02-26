b8757b370d71d1f2bab51bd68111f3a7e802a6fb
cfbatista <camilafreitasbatista@gmail.com>
2020-02-26 13:45:47 -0300

    feat: sign in/login template and validators

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '',
@@ -20,4 +19,5 @@ import { HomePage } from './home.page';
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule {
+}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  authForm: FormGroup;
+  configs = {
+    isLogado: true,
+    action: 'Login',
+    actionChange: 'Cadastro'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(5)]);
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm(): void{
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  get nome() : FormControl {
+    return <FormControl> this.authForm.get('nome');
+  }
+
+  get email() : FormControl {
+    return <FormControl> this.authForm.get('email');
+  }
+
+  get senha() : FormControl {
+    return <FormControl> this.authForm.get('senha');
+  }
+
+  mudarAuthForm(): void {
+    this.configs.isLogado = !this.configs.isLogado;
+
+    const { isLogado } = this.configs;
+
+    this.configs.action = isLogado ? 'Login' : 'Cadastro';
+    this.configs.actionChange = isLogado ? 'Cadastro' : 'Já possui uma conta';
+
+    !isLogado 
+      ? this.authForm.addControl('nome', this.nameControl) 
+      : this.authForm.removeControl('nome');
+  }
+
+  onSubmit() : void{
+    console.log('AuthForm ', this.authForm.value);
+  }
 }
